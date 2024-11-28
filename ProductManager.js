7b69b8a261d3c776808102a216684d6a21cb4b89
JulioBarnes <julitorb96@gmail.com>
2024-11-28 17:53:53 -0300

    1er Ejercicio ProductManager

--- ProductManager.js
+++ ProductManager.js
@@ -15,12 +15,33 @@ class ProductManager {
       code,
       stock,
     };
+
+    const productExist = this.products.find((product) => product.code === code);
+    if (productExist) {
+      return console.log(
+        `El producto con el código ${code} ingresado ya existe`
+      );
+    }
+
+    const propertiesValues = Object.values(newProduct);
+    if (propertiesValues.includes(undefined)) {
+      return console.log("Todos los campos del producto son obligatorios!");
+    }
     this.products.push(newProduct);
   }
 
-  getProduct() {}
+  getProduct() {
+    console.log(this.products);
+  }
 
-  getProductById() {}
+  getProductById(id) {
+    const getProductId = this.products.find((product) => product.id === id);
+    if (!getProductId) {
+      console.log("Producto no encontrado");
+    } else {
+      console.log(getProductId);
+    }
+  }
 }
 
 const products = new ProductManager();
@@ -30,8 +51,52 @@ products.addProduct({
   description: "Descripción del producto 1",
   price: 100,
   thumbnail: "https://example.com/thumbnail1.jpg",
-  code: "123456789",
+  code: "001",
   stock: 10,
 });
 
-console.log(products.products);
+products.addProduct({
+  title: "Producto 2",
+  description: "Descripción del producto 2",
+  price: 100,
+  thumbnail: "https://example.com/thumbnail2.jpg",
+  code: "002",
+  stock: 10,
+});
+
+products.addProduct({
+  title: "Producto 2",
+  description: "Descripción del producto 2",
+  price: 100,
+  thumbnail: "https://example.com/thumbnail2.jpg",
+  code: "002",
+  stock: 10,
+});
+
+products.addProduct({
+  title: "Producto 3",
+  description: "Descripción del producto 3",
+  price: 100,
+  thumbnail: "https://example.com/thumbnail3.jpg",
+  code: "003",
+  stock: 10,
+});
+
+products.addProduct({
+  title: "Producto 4",
+  description: "Descripción del producto 4",
+  price: 100,
+  thumbnail: "https://example.com/thumbnail4.jpg",
+  code: "004",
+});
+
+products.addProduct({
+  title: "Producto 5",
+  description: "Descripción del producto 5",
+  price: 100,
+  thumbnail: "https://example.com/thumbnail5.jpg",
+  code: "005",
+  stock: 10,
+});
+
+products.getProduct();
