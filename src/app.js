03ec99b63bf126022971f862d5263e1b9bbaac83
JulioBarnes <julitorb96@gmail.com>
2024-12-05 22:07:40 -0300

    primer guardado de rama PreEntrega2

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cartsRoutes from "./router/carts.routes.js";
 import productsRoutes from "./router/products.routes.js";
+//
+import handlebars from "express-handlebars";
+import { Server } from "socket.io";
 
 const PORT = 8080;
 
@@ -8,10 +11,25 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", "./src/views");
+app.set("view engine", "handlebars");
+
 app.use("/api/carts", cartsRoutes);
 app.use("/api/products", productsRoutes);
 app.use(express.static("public"));
 
-app.listen(PORT, () => {
+const httpServer = app.listen(PORT, () => {
   console.log(`Servidor escuchando en puerto ${PORT}`);
+});
+
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  console.log("Un usuario conectado");
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
